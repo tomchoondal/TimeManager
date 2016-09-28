20770c926238b0374854191d133c9084a6d5e822
tjose <tjose@VCG-TJOSE>
2016-09-28 11:38:49 +0530

    Added android splash screen. Updated App icons.

--- TimeManager/TimeManager.Droid/Resources/Resource.Designer.cs
+++ TimeManager/TimeManager.Droid/Resources/Resource.Designer.cs
@@ -3321,8 +3321,14 @@ namespace TimeManager.Droid
 			// aapt resource value: 0x7f020062
 			public const int mr_ic_settings_light = 2130837602;
 			
+			// aapt resource value: 0x7f020065
+			public const int notification_template_icon_bg = 2130837605;
+			
 			// aapt resource value: 0x7f020063
-			public const int notification_template_icon_bg = 2130837603;
+			public const int Splash_Screen = 2130837603;
+			
+			// aapt resource value: 0x7f020064
+			public const int SplashIcon = 2130837604;
 			
 			static Drawable()
 			{
